d2b9702f9d8df790ed127c4c979647b23474fabf
asmyasnikov <79263256394@ya.ru>
2022-12-15 03:23:05 +0300

    health_check.sh

--- health_check.sh
+++ health_check.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-/ydb -e grpc://localhost:${GRPC_TLS_PORT:-2136} -d /local scheme ls /local
-/ydb -e grpc://localhost:${GRPC_TLS_PORT:-2136} -d /local table query execute -q 'create table `/local/.sys_health/test` (key int32, value utf8, primary key(key));' -t scheme
+/ydb -e grpcs://localhost:${GRPC_TLS_PORT:-2135} --ca-file /ydb_certs/ca.pem -d /local scheme ls /local
+/ydb -e grpcs://localhost:${GRPC_TLS_PORT:-2135} --ca-file /ydb_certs/ca.pem -d /local table query execute -q 'create table `/local/.sys_health/test` (key int32, value utf8, primary key(key));' -t scheme
